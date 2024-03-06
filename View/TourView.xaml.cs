28ad4b275e98f4b48e5e92f1f85eba5f55efeb1d
mimasekaric <mimasekaric@gmail.com>
2024-03-06 12:43:49 +0100

    -Added TourView.xaml window
    -added TourView.xaml.cs
    -added TourView.xaml
    -added Location field in TourDTO

---
+++ View/TourView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using BookingApp.DTO;
+using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.View
+{
+    /// <summary>
+    /// Interaction logic for TourView.xaml
+    /// </summary>
+    public partial class TourView : Window
+    {
+
+        public static ObservableCollection<TourDTO> Tours { get; set; }
+
+        private readonly TourRepository _repository;
+
+        public TourView()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _repository = new TourRepository();
+            Tours = new ObservableCollection<TourDTO>();
+            Update();
+        }
+        private void Update()
+        {
+            Tours.Clear();
+            foreach (Tour tour in _repository.GetAll()) Tours.Add(new TourDTO(tour));
+        }
+    }
+}
