a41fe481d243caaef57f29f9b354aba80c9f6dad
mimasekaric <mimasekaric@gmail.com>
2024-03-08 13:02:03 +0100

    -Added Search_click function in TourView.xaml.cs
    -added search button and textbox and combobox in TourView.xaml
    -added function for search
    -Overrided ToString() in LocationDTO

--- View/TourView.xaml.cs
+++ View/TourView.xaml.cs
@@ -25,6 +25,7 @@ namespace BookingApp.View
     {
 
         public static ObservableCollection<TourDTO> Tours { get; set; }
+        private static TourDTO tour { get; set; }
 
         private readonly TourRepository _repository;
 
@@ -34,12 +35,88 @@ namespace BookingApp.View
             DataContext = this;
             _repository = new TourRepository();
             Tours = new ObservableCollection<TourDTO>();
+            tour= new TourDTO();
             Update();
         }
         private void Update()
         {
             Tours.Clear();
             foreach (Tour tour in _repository.GetAll()) Tours.Add(new TourDTO(tour));
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = textboxSearch.Text.ToLower();
+            string[] resultArray = searchTerm.Split(',').Select(s => s.Trim()).ToArray();
+
+            if (comboBoxFilter.SelectedItem == null)
+            {
+                MessageBox.Show("You didn't choose an option for filtration!");
+                return;
+            }
+
+            string selectedFilter;
+
+            if (comboBoxFilter.SelectedItem == comboBoxItemDuration)
+            {
+                selectedFilter = comboBoxFilter.SelectedItem.ToString();
+                var filtered = Tours.Where(tour => tour.Duration.ToString().Contains(resultArray[0])).ToList();
+                dataGridTour.ItemsSource = filtered;
+            }
+            else if (comboBoxFilter.SelectedItem == comboBoxItemLanguage)
+            {
+                selectedFilter = comboBoxFilter.SelectedItem.ToString();
+                var filtered = Tours.Where(tour => tour.Language.ToLower().Contains(resultArray[0])).ToList();
+                dataGridTour.ItemsSource = filtered;
+            }
+            else if (comboBoxFilter.SelectedItem == comboBoxItemNumberOfPeople)
+            {
+                
+                    selectedFilter = comboBoxFilter.SelectedItem.ToString();
+                    var filtered = Tours.Where(tour => tour.MaxTouristNumber.ToString().Contains(resultArray[0])).ToList();
+                    dataGridTour.ItemsSource = filtered;
+                
+                
+            }
+            else if(comboBoxFilter.SelectedItem == comboBoxItemPlace)
+            {
+                
+                if (resultArray.Length >= 2)
+                {
+                    string city = resultArray[0];
+                    string country = resultArray[1];
+
+                    var filtered = Tours.Where(tour => tour.LocationDTO.City.ToLower() == city && tour.LocationDTO.Country.ToLower() == country).ToList();
+                    dataGridTour.ItemsSource = filtered;
+                }
+                else if (searchTerm == "")
+                {
+                   
+                    dataGridTour.ItemsSource = Tours;
+                }
+                else
+                {
+                    MessageBox.Show("U didn't input city and contry in right format!");
+                }
+
+
+
+            }
+            else if (comboBoxFilter.SelectedItem == comboBoxItemBeginningTime)
+            { 
+                selectedFilter = comboBoxFilter.SelectedItem.ToString();
+                var filtered = Tours.Where(tour => tour.BeginingTime.ToString().Contains(resultArray[0])).ToList();
+                dataGridTour.ItemsSource = filtered;
+            }
+            else if(comboBoxFilter.SelectedItem == null)
+            {
+                MessageBox.Show("U didn't input anything to search!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid filter selected!");
+            }
+          
         }
     }
 }
