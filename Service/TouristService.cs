43d88be06ea338df2d6a4de6159290bb25f3d04d
jelena-ra <jelena.rdsc02@gmail.com>
2024-04-13 01:10:38 +0200

    - Transffered one window from code-behind to MVVM
    - Added TouristService

---
+++ Service/TouristService.cs
@@ -0,0 +1,38 @@
+﻿using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Service
+{
+    public class TouristService
+    {
+        private TouristRepository _touristRepository = new TouristRepository();
+
+        private TourReservationService _tourReservationService = new TourReservationService();
+
+        public List<Tourist> GetAll()
+        {
+            return _touristRepository.GetAll();
+        }
+
+        public Tourist Save(Tourist tourist)
+        {
+            return _touristRepository.Save(tourist);
+        }
+
+        public void Delete(Tourist tourist)
+        {
+            _touristRepository.Delete(tourist);
+        }
+
+        public Tourist Update(Tourist tourist)
+        {
+            return _touristRepository.Update(tourist);
+        }
+
+    }
+}
