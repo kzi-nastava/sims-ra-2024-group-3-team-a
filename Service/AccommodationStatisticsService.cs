10f91002dbdd70d2f1e9b6c05383d2cc586e896e
nemanajZ02 <nemanja.zekanovic002@gmail.com>
2024-04-30 19:23:49 +0200

    Added Accommodation Statistics
    - added necessary Views
    - added necessary ViewModels
    - added AccommodationStatistics model and service

---
+++ Service/AccommodationStatisticsService.cs
@@ -0,0 +1,88 @@
+﻿using BookingApp.Model.Enums;
+using BookingApp.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace BookingApp.Service
+{
+    public class AccommodationStatisticsService
+    {
+        private IAccommodationReservationRepository _accommodationReservationRepository;
+        private IAccommodationReservationChangeRequestRepository _accommodationReservationChangeRequestRepository;
+
+        public AccommodationStatisticsService(IAccommodationReservationRepository accommodationReservationRepository, IAccommodationReservationChangeRequestRepository accommodationReservationChangeRequestRepository)
+        {
+            _accommodationReservationRepository = accommodationReservationRepository;
+            _accommodationReservationChangeRequestRepository = accommodationReservationChangeRequestRepository;
+        }
+
+        public int GetReservationsNumberByYear(int accommodationId, int year, int month = 0)
+        {
+            int reservations = 0;
+            foreach(var reservation in _accommodationReservationRepository.GetAll())
+            {
+                if (reservation.BeginDate.Year == year && accommodationId == reservation.AccommodationId && (reservation.BeginDate.Month == month || month == 0))
+                {
+                    reservations++;
+                }
+            }
+            return reservations;
+        }  
+        
+        public int GetCancellationsNumberByYear(int accommodationId, int year, int month = 0)
+        {
+            int cancellations = 0;
+            foreach (var reservation in _accommodationReservationRepository.GetAll())
+            {
+                if (reservation.BeginDate.Year == year && accommodationId == reservation.AccommodationId && reservation.Canceled && reservation.Canceled == true && (reservation.BeginDate.Month == month || month == 0))
+                {
+                    cancellations++;
+                }
+            }
+            return cancellations;
+        }
+
+        public int GetAccommodationReservationChangeRequestsByYear(int accommodationId, int year, int month = 0)
+        {
+            int changeRequests = 0;
+            foreach (var changeRequest in _accommodationReservationChangeRequestRepository.GetAll())
+            {
+                if(_accommodationReservationRepository.GetById(changeRequest.AccommodationReservationId).AccommodationId == accommodationId && changeRequest.Status == AccommodationChangeRequestStatus.Accepted && changeRequest.BeginDateNew.Year == year && (changeRequest.BeginDateNew.Month == month || month == 0))
+                {
+                    changeRequests++;
+                }
+            }
+            return changeRequests;
+        }
+
+        public int GetMostOccupiedYear(int accommodationId, int[] years)
+        {
+            int mostOccupiedYear = years[0];
+            foreach(int year in years)
+            {
+                if(GetReservationsNumberByYear(accommodationId, year) > GetReservationsNumberByYear(accommodationId, mostOccupiedYear))
+                {
+                    mostOccupiedYear = year;
+                }
+            }
+            return mostOccupiedYear;
+        }
+        public int GetMostOccupiedMonth(int accommodationId, int year, int[] months)
+        {
+            int mostOccupiedMonth = months[0];
+            foreach (int month in months)
+            {
+                if (GetReservationsNumberByYear(accommodationId, year, month) > GetReservationsNumberByYear(accommodationId, year, mostOccupiedMonth))
+                {
+                    mostOccupiedMonth = month;
+                }
+            }
+            return mostOccupiedMonth;
+        }
+    }
+}
---
+++ ViewModel/Owner/AccommodationStatisticsViewModels/AccommodationStatisticsYearViewModel.cs
@@ -0,0 +1,150 @@
+﻿using BookingApp.Commands;
+using BookingApp.DTO;
+using BookingApp.InjectorNameSpace;
+using BookingApp.Model;
+using BookingApp.Repository.Interfaces;
+using BookingApp.Service;
+using BookingApp.View.Owner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.ViewModel.Owner.AccommodationStatisticsViewModels
+{
+    public class AccommodationStatisticsYearViewModel : ViewModel
+    {
+        private AccommodationStatisticsService _accommodationStatisticsService;
+        private AccommodationDTO _accommodationDTO;
+        private Dictionary<int, AccommodationStatisticsDTO> _accommodationStatisticsDTO;
+
+        private RelayCommand _goBackCommand;
+        private RelayCommand _showSideMenuCommand;
+
+        private int[] _years = { 2022, 2023, 2024, 2025 };
+        private int _selectedYear;
+        private int _mostOccupiedYear;
+
+        public AccommodationStatisticsYearViewModel(AccommodationDTO accommodationDTO)
+        {
+            IAccommodationReservationRepository accommodationResrvationRepository = Injector.CreateInstance<IAccommodationReservationRepository>();
+            IAccommodationReservationChangeRequestRepository accommodationReservationChangeRequestRepository = Injector.CreateInstance<IAccommodationReservationChangeRequestRepository>();
+            _accommodationStatisticsService = new AccommodationStatisticsService(accommodationResrvationRepository, accommodationReservationChangeRequestRepository);
+
+            _accommodationDTO = accommodationDTO;
+            _accommodationStatisticsDTO = new Dictionary<int, AccommodationStatisticsDTO>();
+
+            _goBackCommand = new RelayCommand(GoBack);
+            _showSideMenuCommand = new RelayCommand(ShowSideMenu);
+
+            _mostOccupiedYear = _accommodationStatisticsService.GetMostOccupiedYear(_accommodationDTO.Id, _years);
+            SetStatistics();
+        }
+
+        public int MostOccupiedYear
+        {
+            get
+            {
+                return _mostOccupiedYear;
+            }
+            set
+            {
+                _mostOccupiedYear = value;
+                OnPropertyChanged();
+            }
+        }
+        public int SelectedYear
+        {
+            get
+            {
+                return _selectedYear;
+            }
+            set
+            {
+                _selectedYear = value;
+                OnPropertyChanged();
+                ShowAccommodationStatisticsMonths();
+                _selectedYear = 0;
+            }
+        }
+        public AccommodationDTO AccommodationDTO
+        {
+            get
+            {
+                return _accommodationDTO;
+            }
+            set
+            {
+                _accommodationDTO = value;
+            }
+        }
+
+        public Dictionary<int, AccommodationStatisticsDTO> AccommodationStatisticsDTO
+        {
+            get
+            {
+                return _accommodationStatisticsDTO;
+            }
+            set
+            {
+                _accommodationStatisticsDTO = value;
+            }
+        }
+        public RelayCommand ShowSideMenuCommand
+        {
+            get
+            {
+                return _showSideMenuCommand;
+            }
+            set
+            {
+                _showSideMenuCommand = value;
+                OnPropertyChanged();
+            }
+        }
+        public RelayCommand GoBackCommand
+        {
+            get
+            {
+                return _goBackCommand;
+            }
+            set
+            {
+                _goBackCommand = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private void SetStatistics()
+        {
+            foreach (int year in _years)
+            {
+                AccommodationStatisticsDTO accommodationStatisticsDTO = new AccommodationStatisticsDTO();
+                accommodationStatisticsDTO.Reservations = _accommodationStatisticsService.GetReservationsNumberByYear(_accommodationDTO.Id, year);
+                accommodationStatisticsDTO.Cancellations = _accommodationStatisticsService.GetCancellationsNumberByYear(_accommodationDTO.Id, year);
+                accommodationStatisticsDTO.AccommodationReservationChanges = _accommodationStatisticsService.GetAccommodationReservationChangeRequestsByYear(_accommodationDTO.Id, year);
+
+                if (year == _mostOccupiedYear)
+                {
+                    accommodationStatisticsDTO.IsMostOccupied = true;
+                }
+
+                _accommodationStatisticsDTO.Add(year, accommodationStatisticsDTO);
+            }
+        }
+
+        public void ShowAccommodationStatisticsMonths()
+        {
+            OwnerMainWindow.MainFrame.Content = new AccommodationStatisticsMonthsPage(_accommodationDTO, _selectedYear);
+        }
+        private void ShowSideMenu()
+        {
+            OwnerMainWindow.SideMenuFrame.Content = new SideMenuPage();
+        }
+        private void GoBack()
+        {
+            OwnerMainWindow.MainFrame.GoBack();
+        }
+    }
+}
