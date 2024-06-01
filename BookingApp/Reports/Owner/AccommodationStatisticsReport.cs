db44e1d47e2af737ca2a1df2c23785b5351203a2
nemanajZ02 <nemanja.zekanovic002@gmail.com>
2024-06-01 21:41:16 +0200

    Accommodation photo now can be seen in pdf

--- BookingApp/Reports/Owner/AccommodationStatisticsReport.cs
+++ BookingApp/Reports/Owner/AccommodationStatisticsReport.cs
@@ -1,6 +1,7 @@
 ﻿using BookingApp.DTO;
 using BookingApp.Model;
 using ceTe.DynamicPDF;
+using ceTe.DynamicPDF.Imaging;
 using ceTe.DynamicPDF.PageElements;
 using ceTe.DynamicPDF.PageElements.Charting;
 using ceTe.DynamicPDF.PageElements.Charting.Series;
@@ -8,9 +9,16 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Font = ceTe.DynamicPDF.Font;
+using Image = ceTe.DynamicPDF.PageElements.Image;
+using System.IO;
+using Path = System.IO.Path;
+using FontFamily = ceTe.DynamicPDF.FontFamily;
 
 namespace BookingApp.Reports.Owner
 {
@@ -30,8 +38,51 @@ namespace BookingApp.Reports.Owner
                 Label label = new Label(header, 0, 0, 504, 100, Font.Helvetica, 18, TextAlign.Center);
                 firstPage.Elements.Add(label);
 
+                //slika
+                if (!string.IsNullOrEmpty(accommodation.Images[0]))
+                {
+                    string imagePath = accommodation.Images[0];
+                    if (Path.GetExtension(imagePath).ToLower() == ".png")
+                    {
+                        try
+                        {
+                            string jpgImagePath = ConvertPngToJpg(imagePath);
+                            if (!string.IsNullOrEmpty(jpgImagePath))
+                            {
+                                float scale = GetScale(jpgImagePath);
+                                Image image = new Image(jpgImagePath, 0, 100, scale);
+                                firstPage.Elements.Add(image);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Label errorLabel = new Label("Error loading image: " + ex.Message, 0, 100, 504, 30, Font.Helvetica, 12, TextAlign.Left);
+                            firstPage.Elements.Add(errorLabel);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            float scale = GetScale(imagePath);
+                            Image image = new Image(imagePath, 0, 100, scale);
+                            firstPage.Elements.Add(image);
+                        }
+                        catch (Exception ex)
+                        {
+                            Label errorLabel = new Label("Error loading image: " + ex.Message, 0, 100, 504, 30, Font.Helvetica, 12, TextAlign.Left);
+                            firstPage.Elements.Add(errorLabel);
+                        }
+                    }
+                }
+                else
+                {
+                    Label errorLabel = new Label("Image file not found.", 0, 100, 504, 30, Font.Helvetica, 12, TextAlign.Left);
+                    firstPage.Elements.Add(errorLabel);
+                }
+
                 //grafik1
-                Chart yearlyReservations = new Chart(-30, 120, 600, 300);
+                Chart yearlyReservations = new Chart(-30, 380, 600, 300);
                 PlotArea plotArea = yearlyReservations.PrimaryPlotArea;
                 Title title1 = new Title("Number of reservation");
                 Title title2 = new Title("From year 2022 till year 2025");
@@ -62,7 +113,10 @@ namespace BookingApp.Reports.Owner
                 firstPage.Elements.Add(yearlyReservations);
 
                 //grafik2
-                Chart reservationCancellations = new Chart(-30, 430, 600, 300);
+                Page secondPage = new Page();
+                document.Pages.Add(secondPage);
+
+                Chart reservationCancellations = new Chart(-30, 0, 600, 300);
                 PlotArea plotArea2 = reservationCancellations.PrimaryPlotArea;
                 Title title12 = new Title("Number of reservations canceled");
                 Title title22 = new Title("From year 2022 till year 2025");
@@ -90,13 +144,10 @@ namespace BookingApp.Reports.Owner
                 lineSeries2.YAxis.Interval = 1;
                 lineSeries2.XAxis.LabelFormat = "yyyy.";
                 reservationCancellations.AutoLayout = true;
-                firstPage.Elements.Add(reservationCancellations);
+                secondPage.Elements.Add(reservationCancellations);
 
                 //grafik3
-                Page secondPage = new Page();
-                document.Pages.Add(secondPage);
-
-                Chart reservationChangeRequests = new Chart(0, 0, 500, 300);
+                Chart reservationChangeRequests = new Chart(0, 380, 500, 300);
                 PlotArea plotArea3 = reservationChangeRequests.PrimaryPlotArea;
                 Title title13 = new Title("Number of requests that reservaton should be moved to another date");
                 Title title23 = new Title("From year 2022 till year 2025");
@@ -127,7 +178,10 @@ namespace BookingApp.Reports.Owner
                 secondPage.Elements.Add(reservationChangeRequests);
 
                 // Grafik4
-                Chart renovationRecommendations = new Chart(0, 350, 500, 300);
+                Page thirdPage = new Page();
+                document.Pages.Add(thirdPage);
+
+                Chart renovationRecommendations = new Chart(-30, 0, 500, 300);
                 PlotArea plotArea4 = renovationRecommendations.PrimaryPlotArea;
                 Title title14 = new Title("Number of recommendations for accommodation renovation");
                 Title title24 = new Title("From year 2022 till year 2025");
@@ -155,13 +209,10 @@ namespace BookingApp.Reports.Owner
                 lineSeries4.YAxis.Interval = 1;
                 lineSeries4.XAxis.LabelFormat = "yyyy.";
                 renovationRecommendations.AutoLayout = true;
-                secondPage.Elements.Add(renovationRecommendations);
+                thirdPage.Elements.Add(renovationRecommendations);
 
                 //tabela
-                Page thirdPage = new Page();
-                document.Pages.Add(thirdPage);
-
-                Table2 table = new Table2(-30, 0, 700, 800);
+                Table2 table = new Table2(-30, 380, 700, 800);
                 table.CellDefault.Border.Color = RgbColor.LightBlue;
                 table.CellDefault.Border.LineStyle = LineStyle.Solid;
                 table.CellDefault.Padding.Value = 3.0f;
@@ -225,8 +276,51 @@ namespace BookingApp.Reports.Owner
                 Label label = new Label(header, 0, 0, 504, 100, Font.Helvetica, 18, TextAlign.Center);
                 firstPage.Elements.Add(label);
 
+                //slika
+                if (!string.IsNullOrEmpty(accommodation.Images[0]))
+                {
+                    string imagePath = accommodation.Images[0];
+                    if (Path.GetExtension(imagePath).ToLower() == ".png")
+                    {
+                        try
+                        {
+                            string jpgImagePath = ConvertPngToJpg(imagePath);
+                            if (!string.IsNullOrEmpty(jpgImagePath))
+                            {
+                                float scale = GetScale(jpgImagePath);
+                                Image image = new Image(jpgImagePath, 0, 100, scale);
+                                firstPage.Elements.Add(image);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Label errorLabel = new Label("Error loading image: " + ex.Message, 0, 100, 504, 30, Font.Helvetica, 12, TextAlign.Left);
+                            firstPage.Elements.Add(errorLabel);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            float scale = GetScale(imagePath);
+                            Image image = new Image(imagePath, 0, 100, scale);
+                            firstPage.Elements.Add(image);
+                        }
+                        catch (Exception ex)
+                        {
+                            Label errorLabel = new Label("Error loading image: " + ex.Message, 0, 100, 504, 30, Font.Helvetica, 12, TextAlign.Left);
+                            firstPage.Elements.Add(errorLabel);
+                        }
+                    }
+                }
+                else
+                {
+                    Label errorLabel = new Label("Image file not found.", 0, 100, 504, 30, Font.Helvetica, 12, TextAlign.Left);
+                    firstPage.Elements.Add(errorLabel);
+                }
+
                 //grafik1
-                Chart monthlyReservations = new Chart(-30, 120, 600, 300);
+                Chart monthlyReservations = new Chart(-30, 380, 600, 300);
                 PlotArea plotArea = monthlyReservations.PrimaryPlotArea;
                 Title title1 = new Title("Number of reservation");
                 Title title2 = new Title("From january - december " + yearForMonthReport.ToString());
@@ -258,7 +352,10 @@ namespace BookingApp.Reports.Owner
                 firstPage.Elements.Add(monthlyReservations);
 
                 //grafik2
-                Chart reservationCancellations1 = new Chart(-30, 430, 600, 300);
+                Page secondPage = new Page();
+                document.Pages.Add(secondPage);
+
+                Chart reservationCancellations1 = new Chart(-30, 0, 600, 300);
                 PlotArea plotArea2 = reservationCancellations1.PrimaryPlotArea;
                 Title title12 = new Title("Number of reservations canceled");
                 Title title22 = new Title("From january - december " + yearForMonthReport.ToString());
@@ -287,13 +384,10 @@ namespace BookingApp.Reports.Owner
                 lineSeries2.XAxis.LabelFormat = "MMM";
                 lineSeries2.XAxis.Interval = 1;
                 reservationCancellations1.AutoLayout = true;
-                firstPage.Elements.Add(reservationCancellations1);
+                secondPage.Elements.Add(reservationCancellations1);
 
                 //grafik3
-                Page secondPage = new Page();
-                document.Pages.Add(secondPage);
-
-                Chart reservationChangeRequests = new Chart(0, 0, 500, 300);
+                Chart reservationChangeRequests = new Chart(0, 380, 500, 300);
                 PlotArea plotArea3 = reservationChangeRequests.PrimaryPlotArea;
                 Title title13 = new Title("Number of requests that reservaton should be moved to another date");
                 Title title23 = new Title("From january - december " + yearForMonthReport.ToString());
@@ -325,7 +419,10 @@ namespace BookingApp.Reports.Owner
                 secondPage.Elements.Add(reservationChangeRequests);
 
                 // Grafik4
-                Chart renovationRecommendations = new Chart(0, 350, 500, 300);
+                Page thirdPage = new Page();
+                document.Pages.Add(thirdPage);
+
+                Chart renovationRecommendations = new Chart(-30, 0, 500, 300);
                 PlotArea plotArea4 = renovationRecommendations.PrimaryPlotArea;
                 Title title14 = new Title("Number of recommendations for accommodation renovation");
                 Title title24 = new Title("From january - december " + yearForMonthReport.ToString());
@@ -354,11 +451,11 @@ namespace BookingApp.Reports.Owner
                 lineSeries4.XAxis.LabelFormat = "MMM";
                 lineSeries4.XAxis.Interval = 1;
                 renovationRecommendations.AutoLayout = true;
-                secondPage.Elements.Add(renovationRecommendations);
+                thirdPage.Elements.Add(renovationRecommendations);
 
                 //tabela
-                Page thirdPage = new Page();
-                document.Pages.Add(thirdPage);
+                Page forthPage = new Page();
+                document.Pages.Add(forthPage);
 
                 Table2 table = new Table2(-30, 0, 700, 800);
                 table.CellDefault.Border.Color = RgbColor.LightBlue;
@@ -407,13 +504,48 @@ namespace BookingApp.Reports.Owner
                     tableContent.Cells.Add(new FormattedTextArea(accommodationStatistics.AccommodationRenovationRecommendations.ToString(), 0, 0, 140, 50, FontFamily.Helvetica, 12, false));
                 }
 
-                thirdPage.Elements.Add(table);
+                forthPage.Elements.Add(table);
 
 
                 string reportPath = "../../../../Reports/Owner/Monthly-Report-for-" + accommodation.Name.Replace(" ", "-") + "-" + yearForMonthReport.ToString() + "-" + DateTime.Now.ToString("dd.MM.yyyy.HH.mm") + ".pdf";
                 document.Draw(reportPath);
                 Process.Start("cmd", "/c start " + reportPath);
             }
+        }
+
+        private string ConvertPngToJpg(string pngFilePath)
+        {
+            try
+            {
+                using (Bitmap bmp = new Bitmap(pngFilePath))
+                {
+                    string jpgFilePath = Path.Combine(Path.GetDirectoryName(pngFilePath), Path.GetFileNameWithoutExtension(pngFilePath) + ".jpg");
+                    bmp.Save(jpgFilePath, ImageFormat.Jpeg);
+                    return jpgFilePath;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error converting PNG to JPG: " + ex.Message);
+                return null;
+            }
+        }
+
+        private float GetScale(string jpgImagePath)
+        {
+            float desiredWidth = 400;
+            float desiredHeight = 320;
+
+            System.Drawing.Image originalImage = System.Drawing.Image.FromFile(jpgImagePath);
+            float originalWidth = originalImage.Width;
+            float originalHeight = originalImage.Height;
+
+            float scaleX = desiredWidth / originalWidth;
+            float scaleY = desiredHeight / originalHeight;
+
+            float scale = Math.Min(scaleX, scaleY);
+
+            return scale;
         }
     }
 }
